2e4686de6700fb43a6dbbc9059359bdd64096510
Alexander Anich <alexander.anich@gmail.com>
2015-11-02 13:57:42 -1000

    add file structure && begin oauth

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-
+var auth = require('./routes/auth');
 app.use(express.static('./public'));
+app.use('/auth', auth);
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(PORT, () => {
   console.log('listening on port' + PORT);
