371d3c4a616899ec1f87df05ba89771d905a122c
styel07 <eleazarcorpuz@gmail.com>
2015-11-02 15:26:38 -1000

    add oauth packages

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+'use strict';
 const PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var OAuth2 = require('oauth').OAuth2;
+const oauth2 = module.exports = new OAuth2(
+  process.env.GITHUB_CLIENT_ID,       // client id
+  process.env.GITHUB_CLIENT_SECRET,   // user secret key
+  'https://github.com',               // provider
+  'login/oauth/authorize',            // provider login paths
+  'login/oauth/access_token',         // provider access token path
+  null                                // options
+);
 var auth = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended : true })); // gives the ability body parser
+
 app.use(express.static('./public'));
 app.use('/auth', auth);
 app.get('/', (req, res) => {
---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var oauth2 = require('./../index.js');
+
+// STEP 1: get the providers Auth URL
+router.get('/login', (req,res) => {
+  var authURL = oauth2.getAuthorizeUrl({
+    redirect_uri : 'http://localhost:3000/auth/github/callback',
+    scope : [ 'gist' ],
+    state : 'Authorize' + Math.round(Math.random() * 999999)
+  });
+  res.json({ url : authURL });
+});
+
+// STEP 2: callback from the provider on successful authorization
+router.get('/github/callback', (req,res) => {
+  var code = req.query.code;
+  console.log('this is the code: ',code);
+  if (code === undefined) {
+    return res.status(401).json({ error : 401, message : 'Invalid auth code.' });
+  }
+
+  oauth2.getOAuthAccessToken(
+    code,
+    {
+      redirect_uri : 'http://localhost:3000/auth/github/callback'
+    },
+    (err, access_token, refresh_token, results) => {
+      if (err) {
+        res.status(401).json(err);
+      } else if ( results.error ) {
+        res.status(401).json(results.error);
+      } else { // everthing worked
+        // get token
+        // send token back to client
+        res.json({ access_token : access_token });
+      }
+    });
+});
+
+module.exports = router;
